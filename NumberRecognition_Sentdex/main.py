e663ecd493980a316134671996d1b10d65a75c9c
Ruzil357 <rushilgupta4@gmail.com>
2021-09-12 04:34:18 +0530

    Minst Dataset Number Recognition With Tensorflow

---
+++ NumberRecognition_Sentdex/main.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.utils import normalize
+from os.path import exists
+
+model_name = "NumberRecognition"
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = normalize(x_train, axis=1)
+x_test = normalize(x_test, axis=1)
+
+if exists(model_name):
+    model = load_model(model_name)
+else:
+    model = Sequential()
+    model.add(Flatten())
+    model.add(Dense(128, activation=tf.nn.relu))
+    model.add(Dense(128, activation=tf.nn.relu))
+    model.add(Dense(10, activation=tf.nn.softmax))
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=6)
+
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(f"\nLoss: {round(val_loss, 4)}\nAccuracy: {round(val_acc, 4)}")
+
+model.save(model_name)
